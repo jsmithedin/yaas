34870bb94423b84b73972585d65dacd7f9615602
Jamie Smith <jamies@criticalblue.com>
2016-02-09 17:17:30 +0000

    Add inpendence

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 server.connection({
-    host: '0.0.0.0', 
+    host: '0.0.0.0',
     port: 1314
 });
 
@@ -14,6 +14,16 @@ server.route({
     handler: function(req, reply) {
         reply({
             message: "mon then"
+        }).code( 200 );
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/independence',
+    handler: function(req, reply) {
+        reply({
+            message: "soon..."
         }).code( 200 );
     }
 });
